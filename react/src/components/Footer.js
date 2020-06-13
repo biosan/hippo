87659e8433c07037b2f2869bed8fe3ec985b89f3
biosan <alessandro@biondi.me>
2020-06-13 17:03:14 +0200

    chore(a11y): improve accessibility
    Footer emojis are now wrapped in a span tag and correctly labelled
    
    The 'GitHub corner' link, the a tag, now has some inner text that is readable by screen readers but invisible otherwise

--- react/src/components/Footer.js
+++ react/src/components/Footer.js
@@ -4,8 +4,8 @@ import 'twin.macro';
 function Footer({children}) {
   return (
     <footer>
-        <strong>Built with a 🍺+❤️ by <a href="https://alessandro.biondi.me"><code>@biosan</code></a>.</strong>
-        <p tw="mt-4"><em>Emoji tech stack:</em> 🦀+⚛️</p>
+        <strong>Built with a <span role="img" aria-label="beer">🍺</span>+<span role="img" aria-label="heart">❤️</span> by <a href="https://alessandro.biondi.me"><code>@biosan</code></a>.</strong>
+        <p tw="mt-4"><em>Emoji tech stack:</em> <span role="img" aria-label="crab">🦀</span>+<span role="img" aria-label="atom">⚛️</span></p>
     </footer>
   )
 }
